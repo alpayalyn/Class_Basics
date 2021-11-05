c912c3ffbfefb1517f58bfa9835b3ae302af27c3
alpayalyn <alpay.alin@useinsider.com>
2021-11-05 23:43:01 +0300

    with Parent and child improvements.

--- main.py
+++ main.py
@@ -18,6 +18,18 @@ class Calculator:
     def Summation(self):
         return self.firstNumber + self.secondNumber + Calculator.num
 
+# By typing Calculator.__init__ we could use Summation method, because it went to the Calculator class and put , a and b there.
+# To be able to use a method which is located in parent's class, we used the parameters we want to send in constructor init in the child class.
+class ChildImpl(Calculator):
+    num2 = 200
+
+    def __init__(self):
+        Calculator.__init__(self, 2, 10)
+    
+    # You can benefit from self.num which is default variable in another class/For summation a and b already declared. 
+    def getCompleteData(self):
+        return self.num2 + self.num + self.Summation()
+
 obj = Calculator(2, 3)
 obj.getData()
 print(obj.Summation())
@@ -25,3 +37,6 @@ print(obj.Summation())
 obj1 = Calculator(4, 5)
 obj1.getData()
 print(obj1.Summation())
+
+obj2 = ChildImpl()
+print(obj2.getCompleteData())
